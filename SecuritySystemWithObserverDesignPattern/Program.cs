4d7874a4b1e92d370e21d7d7e39389f518f93731
= <=>
2024-03-17 12:15:50 +0530

    ExternalVisitor class developed

--- SecuritySystemWithObserverDesignPattern/Program.cs
+++ SecuritySystemWithObserverDesignPattern/Program.cs
@@ -1,5 +1,16 @@
 ﻿namespace SecuritySystemWithObserverDesignPattern.ConsoleApplication;
 
+//ExternalVisitor class representing an external visitor who enters/exits the company building
+class ExternalVisitor
+{
+    public int ExternalVisitorId { get; set; }
+    public string ExternalVisitorName { get; set; }
+    public string PurposeOfVisit { get; set; }
+    public DateTime EntryDateTime { get; set; }
+    public DateTime ExitDateTime { get; set; }
+    public bool IsInBuilding { get; set; }
+}
+
 public class Program
 {
     public static void Main(string[] args)
