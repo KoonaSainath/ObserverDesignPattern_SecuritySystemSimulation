2a3e6a4d75ca523b4f919fc8e17d6fecd5a99459
= <=>
2024-03-17 12:12:09 +0530

    Initial structure of Main method

--- SecuritySystemWithObserverDesignPattern/Program.cs
+++ SecuritySystemWithObserverDesignPattern/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SecuritySystemWithObserverDesignPattern.ConsoleApplication;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.ReadKey();
+    }
+}
